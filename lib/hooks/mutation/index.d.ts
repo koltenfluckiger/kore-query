8f648aeb438108bb5be0287b6beb0c052ee5dfb5
Kolten Fluckiger <wrtunder@gmail.com>
2024-01-26 16:54:12 -0700

    updating 1.0.4

--- lib/hooks/mutation/index.d.ts
+++ lib/hooks/mutation/index.d.ts
@@ -2,10 +2,31 @@ import { KoreQueryContext } from "../../providers";
 import { QueryClient } from "@tanstack/react-query";
 import React from "react";
 import type { UseMutationResult } from "@tanstack/react-query";
+/**
+ * Interface for options used in useKoreQueryMutation hook.
+ * @typedef {Object} Options
+ * @property {() => Promise<any>} [mutationFn] - Optional mutation function to be executed.
+ * @property {React.Context<QueryClient | undefined>} queryContext - React context providing QueryClient.
+ */
+/**
+ * Custom hook for handling mutations with React Query.
+ *
+ * @param {Object} params - Parameters for the hook.
+ * @param {typeof KoreQueryContext | undefined} params.queryContext - Context providing QueryClient, defaults to useKoreQueryContext.
+ * @param {() => Promise<any>} params.mutationFn - Mutation function to be executed.
+ * @returns {UseMutationResult} - The result of the useMutation hook from React Query.
+ */
 declare function useKoreQueryMutation({ queryContext, mutationFn, }: {
     queryContext: typeof KoreQueryContext | undefined;
     mutationFn: Awaited<Promise<any>>;
 }): UseMutationResult;
+/**
+ * Custom hook for creating a mutation request with React Query.
+ *
+ * @param {Object} params - Parameters for the hook.
+ * @param {React.Context<QueryClient | undefined>} params.queryContext - Context providing QueryClient, defaults to useKoreQueryContext.
+ * @returns {UseMutationResult} - The result of the useMutation hook configured for the request.
+ */
 declare function useKoreQueryMutationRequest({ queryContext, }: {
     queryContext: React.Context<QueryClient | undefined>;
 }): UseMutationResult<any, unknown, any, unknown>;
--- lib/hooks/query/index.d.ts
+++ lib/hooks/query/index.d.ts
@@ -1,11 +1,37 @@
 import { QueryClient } from "@tanstack/react-query";
 import React from "react";
+/**
+ * Options for useKoreQuery hook.
+ * @typedef {Object} Options
+ * @property {Array<string>} queryKey - Unique identifier for the query.
+ * @property {() => Promise<any>} queryFunc - Function to fetch the data.
+ * @property {React.Context<QueryClient | undefined>} queryContext - React context providing QueryClient.
+ * @property {Object} queryOptions - Additional options for the query.
+ */
+/**
+ * Custom hook for simplified data fetching using React Query.
+ *
+ * @param {Object} params - Parameters for the hook.
+ * @param {React.Context<QueryClient | undefined>} [params.queryContext=useKoreQueryContext()] - Context providing QueryClient.
+ * @param {Array<string>} params.queryKey - Unique key for the query.
+ * @param {() => Promise<any>} params.queryFunc - Function to execute for data fetching.
+ * @param {Object} [params.queryOptions] - Additional options for the query.
+ * @returns {Object} - The result of the useQuery hook from React Query.
+ */
 declare function useKoreQuery({ queryContext, queryKey, queryFunc, queryOptions, }: {
     queryContext: React.Context<QueryClient | undefined>;
     queryKey: Array<string>;
     queryFunc: Awaited<Promise<any>>;
     queryOptions: Object;
 }): import("@tanstack/react-query").UseQueryResult<any, unknown>;
+/**
+ * Custom hook for automatically refetching queries at a specified interval.
+ *
+ * @param {Object} params - Parameters for the hook.
+ * @param {number} [params.refetchInterval=Time.convert(...)] - Interval for auto refetching in milliseconds.
+ * @param {...otherParams} - Other parameters passed to useKoreQuery.
+ * @returns {Object} - The result of the useQuery hook with auto refetching configured.
+ */
 declare function useKoreQueryAutoRefetch({ refetchInterval, queryContext, queryKey, queryFunc, }: {
     refetchInterval: number | undefined;
     queryContext: React.Context<QueryClient | undefined>;
